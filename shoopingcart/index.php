92738c0984bbcfe57adafee1785d30000fccf2c7
Goran <goran.dam@gmail.com>
2017-03-17 18:37:14 +0100

    Shooping cart wwith seasons

---
+++ shoopingcart/index.php
@@ -0,0 +1,59 @@
+<?php 
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/magicquotes.inc.php';
+
+$items = array(
+	array('id' => '1', 'desc' => 'Canadian-Australian Dictionary', 'price' => 24.95),
+	array('id' => '2', 'desc' => 'As-new parachute (never opened)', 'price' => 1000),
+	array('id' => '3', 'desc' => 'Songs of the Goldfish (2CD set)', 'price' => 19.99),
+	array('id' => '4', 'desc' => 'Simply JavaScript (SitePoint)', 'price' => 39.95));
+	
+// Sessions code
+session_start();
+if (!isset($_SESSION['cart'])) 
+{
+	$_SESSION['cart'] = array();
+}
+
+
+// Insert session code
+if (isset($_POST['action']) and $_POST['action'] == 'Buy') {
+	
+	//Add items at the end of $_SESSION['cart'] array
+	$_SESSION['cart'] [] = $_POST['id'];
+	header('Location: .');
+	exit();
+}
+
+
+// Empty card code
+if (isset($_POST['action']) and $_POST['action'] == 'Empty cart') {
+	
+	//Empty the $_SESSION['cart'] array
+	unset($_SESSION['cart']);
+	header('Location: ?cart');
+	exit();
+}
+	
+	
+// Output checkaut(card) code
+if (isset($_GET['cart'])) {
+	
+	$cart = array();
+	$total = 0;
+	foreach ($_SESSION['cart'] as $id) {
+		
+		foreach ($items as $product) {
+			
+			if ($product['id'] == $id) {
+				
+				$cart[] = $product;
+				$total += $product['price'];
+				break;
+			}
+		}
+	}
+	include 'cart.html.php';
+	exit();
+}
+
+include 'catalog.html.php';
